c333bf2c7ae69dbc4b8b34bebb3a490a2d4fed32
Steven Richart <steven.richart@laposte.net>
2022-01-28 14:06:08 +0100

    ajout du eslint des image du formulaire et mise a jour du script pour la page index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,28 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+	const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	function getUserCardDOM() {
+		const article = document.createElement("article");
+		const img = document.createElement("img");
+		img.setAttribute("alt", name);
+		img.setAttribute("src", picture);
+		const h2 = document.createElement("h2");
+		h2.textContent = name;
+		const h1 = document.createElement("h1");
+		h1.textContent = city + ", " + country;
+		const p1 = document.createElement("p");
+		p1.textContent = tagline;
+		const p2 = document.createElement("p");
+		p2.textContent = price + "€/jour";
+		article.appendChild(lien);
+		lien.appendChild(img);
+		lien.appendChild(h2);
+		article.appendChild(h1);
+		article.appendChild(p1);
+		article.appendChild(p2);
+		return article;
+	}
+	return { name, portrait, city, country, tagline, price, getUserCardDOM };
 }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,95 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+	const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+	const modal = document.getElementById("contact_modal");
+	modal.style.display = "none";
+}
+
+//form const verification
+const form = document.getElementById("form");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const mail = document.getElementById("email");
+
+//const affichage erreur
+const firstErrorMsg = document.getElementById("first-error-msg");
+const lastErrorMsg = document.getElementById("last-error-msg");
+const mailErrorMsg = document.getElementById("email-error-msg");
+
+//empeche l'action par defaut du bouton submit
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+});
+//form etape verification
+function validate() {
+	let firstChecked;
+	let lastChecked;
+	let emailChecked;
+
+	//form verifie que le prénom possède bien 2 caractères et que se soit bien des lettre
+	if (
+		!first.value.match(/(.*[a-z]){2}/i) ||
+		first.value == " " ||
+		first.value.length < 2
+	) {
+		//si non affiche les erreur
+		firstErrorMsg.innerText =
+			"Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
+		firstErrorMsg.style.color = "#691616";
+		firstErrorMsg.style.fontSize = "2rem";
+		firstErrorMsg.style.marginTop = "10px";
+		first.style.border = "solid #691616 2px";
+		firstErrorMsg.style.display = "block";
+		firstChecked = false;
+	} else {
+		//si oui enlève les erreur
+		firstErrorMsg.style.display = "none";
+		first.style.border = "none";
+		firstChecked = true;
+	}
+	//form verifie que le nom possède bien 2 caractères et que se soit bien des lettre
+	if (
+		!last.value.match(/(.*[a-z]){2}/i) ||
+		last.value == " " ||
+		last.value.length < 2
+	) {
+		//si non affiche les erreur
+		lastErrorMsg.innerText =
+			"Veuillez entrer 2 caractères ou plus pour le champ du Nom.";
+		lastErrorMsg.style.color = "#691616";
+		lastErrorMsg.style.fontSize = "2rem";
+		lastErrorMsg.style.marginTop = "10px";
+		last.style.border = "solid #691616 2px";
+		lastErrorMsg.style.display = "block";
+		lastChecked = false;
+	} else {
+		//si oui enlève les erreur
+		lastErrorMsg.style.display = "none";
+		last.style.border = "none";
+		lastChecked = true;
+	}
+	//form verifie que le l'email soit bien un format email
+	if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value)) {
+		//si non affiche les erreur
+		mailErrorMsg.innerText = "Veuillez entrer une email valide";
+		mailErrorMsg.style.color = "#691616";
+		mailErrorMsg.style.fontSize = "2rem";
+		mailErrorMsg.style.marginTop = "10px";
+		mailErrorMsg.style.display = "block";
+		mail.style.border = "solid #691616 2px";
+		emailChecked = false;
+	} else {
+		//si oui enlève les erreur
+		mailErrorMsg.style.display = "none";
+		mail.style.border = "none";
+		emailChecked = true;
+	}
+	//verifie que chaque champ du formulaire est valide
+	if (firstChecked == true && lastChecked == true && emailChecked == true) {
+		form.style.display = "none";
+		valid.style.display = "flex";
+	}
 }
