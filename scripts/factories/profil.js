a2b4b4cc66c033c22d563390f5f39d435244acb6
Steven Richart <steven.richart@laposte.net>
2022-02-02 12:47:15 +0100

    ajout de la supression des article en trop

--- scripts/factories/profil.js
+++ scripts/factories/profil.js
@@ -9,16 +9,12 @@ function profilFactory(data, profilid) {
 		if (id == profilid) {
 			//regarde si oui il les ajoute au document
 			const article = document.createElement("article");
-			const img = document.createElement("img");
-			img.setAttribute("alt", name);
-			img.setAttribute("src", picture);
 			const h2 = document.createElement("h2");
 			h2.textContent = name;
 			const h1 = document.createElement("h1");
 			h1.textContent = city + ", " + country;
 			const p1 = document.createElement("p");
 			p1.textContent = tagline;
-			article.appendChild(img);
 			article.appendChild(h2);
 			article.appendChild(h1);
 			article.appendChild(p1);
@@ -26,9 +22,37 @@ function profilFactory(data, profilid) {
 		} else {
 			//regarde si non il ajoute un article en display none pour eviter les erreur
 			const article = document.createElement("article");
-			article.setAttribute("display", "none");
+			article.setAttribute("style", "display:none");
+			article.setAttribute("class", "supp");
 			return article;
 		}
 	}
-	return { name, id, portrait, city, country, tagline, getUserCardDOM };
+	function getUserCardDOM2() {
+		//regarde si l'id correspond a celui de la page
+		if (id == profilid) {
+			//regarde si oui il les ajoute au document
+			const article = document.createElement("article");
+			const img = document.createElement("img");
+			img.setAttribute("alt", name);
+			img.setAttribute("src", picture);
+			article.appendChild(img);
+			return article;
+		} else {
+			//regarde si non il ajoute un article en display none pour eviter les erreur
+			const article = document.createElement("article");
+			article.setAttribute("style", "display:none");
+			article.setAttribute("class", "supp");
+			return article;
+		}
+	}
+	return {
+		name,
+		id,
+		portrait,
+		city,
+		country,
+		tagline,
+		getUserCardDOM,
+		getUserCardDOM2,
+	};
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -11,6 +11,7 @@ async function getPhotographers() {
 
 async function displayData(photographers) {
 	const photographersHeader = document.querySelector(".photograph-header");
+	const photographersimg = document.querySelector(".photograph-image");
 	let url_str = document.URL;
 
 	let url = new URL(url_str);
@@ -21,14 +22,39 @@ async function displayData(photographers) {
 		const photographerModel = profilFactory(photographer, profilid);
 		const userCardDOM = photographerModel.getUserCardDOM();
 		photographersHeader.appendChild(userCardDOM);
+
+		const photographerModelimg = profilFactory(photographer, profilid);
+		const userCardDOM2 = photographerModelimg.getUserCardDOM2();
+		photographersHeader.appendChild(userCardDOM2);
 	});
 }
 
+//gestion des supression des inutile ou de se que on demande
+Element.prototype.remove = function () {
+	this.parentElement.removeChild(this);
+};
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+	for (var i = this.length - 1; i >= 0; i--) {
+		if (this[i] && this[i].parentElement) {
+			this[i].parentElement.removeChild(this[i]);
+		}
+	}
+};
+//surprime la class demander du dom
+function removeclass(classname) {
+	document.getElementsByClassName(classname).remove();
+}
+//surprime le id demander du dom
+function removeid(id) {
+	document.getElementById(id).remove();
+}
 async function init() {
 	// Récupère les datas des photographes
 	const { photographers } = await getPhotographers();
 	console.log(photographers);
 	displayData(photographers);
+	//surprime les article en trop
+	removeclass("supp");
 }
 
 init();
